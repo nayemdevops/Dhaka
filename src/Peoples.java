a7de5daafc05e60729115630889bdb3b14782324
Md Ashraful Islam Nayem <nayemdevops@gmail.com>
2024-10-14 13:49:03 -0400

    2nd commit

---
+++ src/Peoples.java
@@ -0,0 +1,6 @@
+public class Peoples {
+    public static void main(String[] args) {
+        System.out.println("About 190 Million Peoples Live Here!");
+
+    }
+}
---
+++ src/PlaceToVisit.java
@@ -0,0 +1,8 @@
+public class PlaceToVisit {
+    public static void main(String[] args) {
+        System.out.println("Lalbag Mosque");
+        System.out.println("Shaheed Minar");
+        System.out.println("Old Dhaka");
+
+    }
+}
